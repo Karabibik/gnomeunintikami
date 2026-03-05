91be818eec5f460d2d400aa4a68521760fb9d9cf
Karabibik <8554403+Karabibik@users.noreply.github.com>
2026-03-05 16:45:34 +0300

    Update base URL for graph JSON retrieval

--- docs/javascripts/interactive_graph.js
+++ docs/javascripts/interactive_graph.js
@@ -85,9 +85,7 @@ function graph_nodes() {
   return option.series[0].data.filter(it => [...new Set(ids)].includes(it.id));
 }
 
-const base_url = window.location.pathname.split('/gnomeunintikami/')[0] + '/gnomeunintikami/';
-
-$.getJSON("${base_url}assets/javascripts/graph.json", function (graph) {
+$.getJSON("/assets/javascripts/graph.json", function (graph) {
   myChart.hideLoading();
 
   // an offset of 5, so the dot/node is not that small
